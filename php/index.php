3b548932d530e49e991f549188196890993e604b
Carlos Tadeu <carlinhos.slz@hotmail.com>
2023-10-31 00:05:04 -0300

    update index.php

--- php/index.php
+++ php/index.php
@@ -7,13 +7,18 @@ session_start();
 
 include './conection.php';
 
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if (isset($_POST['useremail']) && isset($_POST['password'])) {
-    $useremail = $_POST['useremail'];
+if (!$conn) {
+    die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
+}
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
     // Consulta para verificar as credenciais
-    $query = "SELECT id, email, senhaCriptografada FROM access WHERE email = '$useremail'";
+    $query = "SELECT id, nome, senhaCriptografada FROM access WHERE email = '$email'";
     $result = mysqli_query($conn, $query);
 
     if ($result && mysqli_num_rows($result) > 0) {
@@ -26,14 +31,14 @@ if (isset($_POST['useremail']) && isset($_POST['password'])) {
             $_SESSION['user_name'] = $row['nome'];
             header("Location: home.php");
         } else {
-            echo "Credenciais inválidas.";
+            echo "Credenciais inválida.";
         }
     } else {
         echo "Credenciais inválidas.";
     }
 }
 
-//mysqli_close($conn);
+mysqli_close($conn);
 
 
 ?>
